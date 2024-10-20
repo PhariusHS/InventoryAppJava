7ac01e3d653400be6a5f4d6b2510d43e5dfcbe26
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2024-10-20 20:28:28 +0200

    Initializing project

---
+++ src/test/java/com/pharius/inventoryapp/inventoryapp/InventoryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharius.inventoryapp.inventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
