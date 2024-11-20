68ecc3b28d69fe7fd2be840b664ab91633e4895e
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2024-11-20 22:17:05 +0100

    Fixed Entity product

--- src/main/java/com/pharius/inventoryapp/inventoryapp/Entities/Product.java
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Entities/Product.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
@@ -14,28 +20,4 @@ public class Product {
     private String name;
     private int stock;
 
-
-    //Getters and Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
-    } 
-
-
-
-    
 }
