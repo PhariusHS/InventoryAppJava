9b193d03d6f183cc4d398e268cfa8d26e6e71a45
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2024-12-16 22:29:53 +0100

    Add restock services and repository

--- src/main/java/com/pharius/inventoryapp/inventoryapp/Models/ProductModels/Product.java
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Models/ProductModels/Product.java
@@ -2,14 +2,11 @@ package com.pharius.inventoryapp.inventoryapp.Models.ProductModels;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
