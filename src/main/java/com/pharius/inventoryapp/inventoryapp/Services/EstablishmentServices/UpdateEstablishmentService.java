b996d5830034d6fd9c274485b116791551a42e91
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2025-01-21 15:51:47 +0100

    Add inventoryServices update EstablishmentController

--- src/main/java/com/pharius/inventoryapp/inventoryapp/Services/EstablishmentServices/UpdateEstablishmentService.java
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Services/EstablishmentServices/UpdateEstablishmentService.java
@@ -1,8 +1,41 @@
 package com.pharius.inventoryapp.inventoryapp.Services.EstablishmentServices;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.pharius.inventoryapp.inventoryapp.Controllers.Command;
+import com.pharius.inventoryapp.inventoryapp.Models.EstablishmentModels.Establishment;
+import com.pharius.inventoryapp.inventoryapp.Models.EstablishmentModels.EstablishmentDTO;
+import com.pharius.inventoryapp.inventoryapp.Repositories.EstablishmentRepository;
+
 @Service
-public class UpdateEstablishmentService {
+public class UpdateEstablishmentService implements Command<UpdateEstablishmentCommand, EstablishmentDTO> {
+
+    private final EstablishmentRepository establishmentRepository;
+
+    public UpdateEstablishmentService(EstablishmentRepository establishmentRepository){
+        this.establishmentRepository = establishmentRepository;
+    }
+
+    @Override
+    public ResponseEntity<EstablishmentDTO> execute(UpdateEstablishmentCommand updateEstablishmentCommand) {
+
+        Optional<Establishment> foundedEstablishment = establishmentRepository.findById(updateEstablishmentCommand.getId());
+
+        if(foundedEstablishment.isPresent()){
+
+        Establishment existingEstablishment = foundedEstablishment.get();
+        existingEstablishment.setEstablishmentId(updateEstablishmentCommand.getId());
+        Establishment updatedEstablishment = establishmentRepository.save(existingEstablishment);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new EstablishmentDTO(updatedEstablishment));
+        }
+
+        return null;//TODO: Validation and handling
+    }
     
+
 }
---
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Services/InventoryServices/DeleteInventoryProductService.java
@@ -0,0 +1,29 @@
+package com.pharius.inventoryapp.inventoryapp.Services.InventoryServices;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.pharius.inventoryapp.inventoryapp.Controllers.Command;
+import com.pharius.inventoryapp.inventoryapp.Models.InventoryModels.InventoryProducts;
+import com.pharius.inventoryapp.inventoryapp.Repositories.InventoryProductsRepository;
+
+public class DeleteInventoryProductService implements Command<Long, InventoryProducts> {
+
+    private final InventoryProductsRepository inventoryProductsRepository;
+
+    public DeleteInventoryProductService(InventoryProductsRepository inventoryProductsRepository) {
+        this.inventoryProductsRepository = inventoryProductsRepository;
+    }
+
+    @Override
+    public ResponseEntity<InventoryProducts> execute(Long inventoryProductId) {
+        Optional<InventoryProducts> foundedInventoryProduct = inventoryProductsRepository.findById(inventoryProductId);
+        if (foundedInventoryProduct.isPresent()) {
+            inventoryProductsRepository.deleteById(inventoryProductId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return null; //TODO error handling and validation
+    }
+}
