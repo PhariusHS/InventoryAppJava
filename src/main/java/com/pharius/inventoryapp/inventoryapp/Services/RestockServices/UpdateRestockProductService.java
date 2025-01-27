7819054a1802208df14e1de8a467c5841609111d
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2025-01-27 09:19:09 +0100

    Fixed beans errors

--- src/main/java/com/pharius/inventoryapp/inventoryapp/Services/RestockServices/UpdateRestockProductService.java
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Services/RestockServices/UpdateRestockProductService.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.pharius.inventoryapp.inventoryapp.Controllers.Command;
 import com.pharius.inventoryapp.inventoryapp.Models.RestockModels.RestockProduct;
 import com.pharius.inventoryapp.inventoryapp.Repositories.RestockProductRepository;
 
+@Service
 public class UpdateRestockProductService implements Command<UpdateRestockProductCommand, RestockProduct> {
     
    private final RestockProductRepository restockProductRepository;
@@ -23,17 +25,12 @@ public class UpdateRestockProductService implements Command<UpdateRestockProduct
         Optional<RestockProduct> foundedRestockProduct = restockProductRepository.findById(updateRestockProductCommand.getId());
 
         if (foundedRestockProduct.isPresent()){
-
             RestockProduct existingRestockProduct = foundedRestockProduct.get();
             existingRestockProduct.setId(updateRestockProductCommand.getId());
             RestockProduct updatedRestockProduct = restockProductRepository.save(existingRestockProduct);
-
             return ResponseEntity.status(HttpStatus.OK).body(new RestockProduct(updatedRestockProduct));
         }
         return null; // TODO: error handling and validation
     }
-
-    
-
 
 }
