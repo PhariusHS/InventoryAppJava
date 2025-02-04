6524b25348cd336defa1b193a0f1b481a04ff70e
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2025-02-04 10:46:22 +0100

    Removed unused imports

--- src/main/java/com/pharius/inventoryapp/inventoryapp/Services/RestockServices/CreateRestockProductService.java
+++ src/main/java/com/pharius/inventoryapp/inventoryapp/Services/RestockServices/CreateRestockProductService.java
@@ -10,15 +10,11 @@ import com.pharius.inventoryapp.inventoryapp.Controllers.DoubleRelationalCommand
 import com.pharius.inventoryapp.inventoryapp.Exceptions.EntityNotFoundException;
 import com.pharius.inventoryapp.inventoryapp.Exceptions.ErrorMessages;
 import com.pharius.inventoryapp.inventoryapp.Exceptions.QuantityNotAvailableException;
-import com.pharius.inventoryapp.inventoryapp.Models.InventoryModels.Inventory;
 import com.pharius.inventoryapp.inventoryapp.Models.InventoryModels.InventoryProducts;
-import com.pharius.inventoryapp.inventoryapp.Models.ProductModels.Product;
 import com.pharius.inventoryapp.inventoryapp.Models.RestockModels.Restock;
 import com.pharius.inventoryapp.inventoryapp.Models.RestockModels.RestockProduct;
 import com.pharius.inventoryapp.inventoryapp.Models.RestockModels.RestockProductDTO;
 import com.pharius.inventoryapp.inventoryapp.Repositories.InventoryProductsRepository;
-import com.pharius.inventoryapp.inventoryapp.Repositories.InventoryRepository;
-import com.pharius.inventoryapp.inventoryapp.Repositories.ProductRepository;
 import com.pharius.inventoryapp.inventoryapp.Repositories.RestockProductRepository;
 import com.pharius.inventoryapp.inventoryapp.Repositories.RestockRepository;
 
@@ -29,15 +25,12 @@ public class CreateRestockProductService
     private final RestockProductRepository restockProductRepository;
     private final InventoryProductsRepository inventoryProductsRepository;
     private final RestockRepository restockRepository;
-    private final InventoryRepository inventoryRepository;
 
     public CreateRestockProductService(RestockProductRepository restockProductRepository,
-            InventoryProductsRepository inventoryProductsRepository, RestockRepository restockRepository,
-            InventoryRepository inventoryRepository) {
+            InventoryProductsRepository inventoryProductsRepository, RestockRepository restockRepository) {
         this.restockProductRepository = restockProductRepository;
         this.inventoryProductsRepository = inventoryProductsRepository;
         this.restockRepository = restockRepository;
-        this.inventoryRepository = inventoryRepository;
     }
 
     @Override
